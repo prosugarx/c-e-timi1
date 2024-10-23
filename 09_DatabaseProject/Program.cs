595e6e59bb07f6fb9877049c2e9d32dcc28d8add
rumeysavarli23@gmail.com <rumeysavarli23@gmail.com>
2024-10-23 22:18:30 +0300

    09_dataType

--- 09_DatabaseProject/Program.cs
+++ 09_DatabaseProject/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,37 @@ namespace _09_DatabaseProject
     {
         static void Main(string[] args)
         {
+            //Ado.net sql i kullanmak için bir çerçeve
+
+            Console.WriteLine("*****c# veri tabanlı ürün-kategori bilgi sistemi*****");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("*******************************************************");
+            string tableNumber;
+            Console.WriteLine("1-kategori");
+            Console.WriteLine("2-ürün");
+            Console.WriteLine("3-siparişler");
+            Console.WriteLine("4-çıkış yap");
+            Console.Write("lütfen girmek istediğiniz tablo numarasını giriniz:");
+            tableNumber = Console.ReadLine();
+            Console.WriteLine("*******************************************************");
+
+            SqlConnection connection= new SqlConnection("Data Source=RUMEYSA\\SQLEXPRESS; initial Catalog= c#DataBaseÖğrenme; integrated security=true");
+            connection.Open();
+            SqlCommand commend=new SqlCommand("Select * From TblCatagory", connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(commend);
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            connection.Close();
+            foreach (DataRow row in dataTable.Rows)
+            {
+                foreach (var item in row.ItemArray)
+                {
+                    Console.Write(item.ToString());
+                }
+                Console.WriteLine();
+            }
+            Console.Read();
         }
     }
 }
